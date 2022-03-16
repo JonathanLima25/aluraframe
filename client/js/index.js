25d2b06f4829ea8565195a5a9883b64c8976c5a0
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-03-16 14:20:46 -0300

    dynamically add items tothe

---
+++ client/js/index.js
@@ -0,0 +1,26 @@
+let campos = [
+    document.querySelector('#data'),
+    document.querySelector('#quantidade'),
+    document.querySelector('#valor')
+];
+
+console.log(campos);
+
+let tbody = document.querySelector('table tbody');
+document.querySelector('.form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    let tr = document.createElement('tr');
+
+    campos.forEach((campo) => {
+        let td = document.createElement('td');
+        td.textContent = campo.value;
+        tr.appendChild(td);
+    });
+
+    let tdVolume = document.createElement('td');
+    tdVolume.textContent = campos[1].value * campos[2].value;
+
+    tr.appendChild(tdVolume);
+    tbody.appendChild(tr);
+
+})
