8c29c81303dbf255d48962386e1c761fa926fb43
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-03-17 17:23:41 -0300

    add method static

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,10 +1,13 @@
 class DateHelper {
+    constructor() {
+        throw new Error("DateHelper não pode ser instanciada.");
+    }
     
-    dataParaTexto(data) {
+    static dataParaTexto(data) {
         return `${data.getDate()}/${data.getMonth()+1}/${data.getFullYear()}`;
     }
 
-    textoParaData(texto) {
+    static textoParaData(texto) {
         return new Date(...texto.split('-').map((item, index) => item - index % 2)); 
     }
 }
