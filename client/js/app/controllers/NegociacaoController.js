04e1f1a7fb25aef6fea25a431b7d24318606721a
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-03-21 11:14:58 -0300

    create proxy

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -6,6 +6,21 @@ class NegociacaoController {
         this._inputQuantidade = $('#quantidade');
         this._inputValor = $('#valor');
 
+        let self = this;
+
+        this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+            get(target, prop, receiver){
+                if(['adiciona', 'esvazia'].includes(prop) && typeof(target[prop])==typeof(Function)){
+                    return function() {
+                        console.log(`Interceptando ${prop}`);
+                        Reflect.apply(target[prop], target, arguments);
+                        self._negociacoesView.update(target);
+                    }
+                }
+                return Reflect.get(target, prop, receiver);
+            }
+        })
+
         // this._listaNegociacoes = new ListaNegociacoes(model => this._negociacoesView.update(model));
 
         this._negociacoesView = new NegociacoesView($('#negociacoesView'));
