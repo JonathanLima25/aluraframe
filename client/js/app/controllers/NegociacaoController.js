4bfb2380a1134fc7354bc587c613c9f9a1dc36f4
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-03-22 10:56:02 -0300

    import data from server

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -32,6 +32,27 @@ class NegociacaoController {
         console.log(this._listaNegociacoes.negociacoes);
     }
 
+    importaNegociacoes() {
+        let xhr = new XMLHttpRequest();
+
+        xhr.open('GET', 'negociacoes/semana');
+
+        xhr.onreadystatechange = () => {
+            if(xhr.readyState == 4) {
+                if(xhr.status == 200) {
+                    console.log("Obtendo as negociações do servidor");
+                    console.log(xhr.responseText);
+                } else {
+                    console.log("Não foi possível obter as negociações");
+                    console.log(xhr.responseText);
+                }
+
+            }
+        }
+
+        xhr.send();
+    }
+
     apaga() {
         this._listaNegociacoes.esvazia();      
         this._mensagem.texto = "Negociações apagadas com sucesso!";
