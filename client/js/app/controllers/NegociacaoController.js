2ee44b4e3dc9f8f11f65f91b8ca62d4745213ba3
JonathanLima25 <jonathan.ferreira6@fatec.sp.gov.br>
2022-03-22 10:58:03 -0300

    transform data

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -41,7 +41,7 @@ class NegociacaoController {
             if(xhr.readyState == 4) {
                 if(xhr.status == 200) {
                     console.log("Obtendo as negociações do servidor");
-                    console.log(xhr.responseText);
+                    console.log(JSON.parse(xhr.responseText));
                 } else {
                     console.log("Não foi possível obter as negociações");
                     console.log(xhr.responseText);
